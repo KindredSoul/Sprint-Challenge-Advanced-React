deea443d9fc44864436d218eac01321c69cc4451
Jabril Breckenridge <jabriltb2@gmail.com>
2019-11-10 22:09:06 -0600

    Initial commit. MVP 50% complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		wwcData: []
+	};
+	componentDidMount() {
+		const getData = () => {
+			fetch("http://localhost:5000/api/players")
+				.then(res => res.json())
+				.then(res => {
+					console.log(res);
+					this.setState({
+						wwcData: res
+					});
+				})
+				.catch(err => console.log(err));
+		};
+		getData();
+	}
+	render() {
+		return (
+			<div>
+				{this.state.wwcData.map(data => {
+					return (
+						<div key={data.id}>
+							<h2>Name: {data.name}</h2>
+							<h3>Country: {data.country}</h3>
+							<h3>Times searhced: {data.searches}</h3>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
